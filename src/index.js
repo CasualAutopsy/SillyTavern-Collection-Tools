89a40cee71bf16fb1d4a58614f9d44220a9f4032
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-04-05 09:17:09 -0400

    Update `index.js`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import {registerMutableSlashCommands} from "./commands/mutable/registry.js";
+import {registerMutableSlashCommands} from './commands/mutable/registry.js';
+import {registerRandomSlashCommands} from './commands/random/registry.js';
+
+import {registerRandomCollectionMacros} from './macros/random/registry.js';
+import {registerMutableListMacros} from './macros/mutable/registry.js';
+
 
 
 registerMutableSlashCommands();
+registerRandomSlashCommands();
+
+registerRandomCollectionMacros();
+registerMutableListMacros();
